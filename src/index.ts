4352ca624321a4ab477077e7bc0944d43398b9f7
nycode802 <nycode802@users.noreply.github.com>
2025-08-02 19:35:08 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = "https://example.com";
+		const destinationURL = "https://zola.com/weddings/matthewandangelina2026";
 		const statusCode = 301;
 		return Response.redirect(destinationURL, statusCode);
 	},
